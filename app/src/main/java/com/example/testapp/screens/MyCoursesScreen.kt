c1412aca0ebd52e504f2d39f662daca0fca7b2f7
SaurabhKushwaha001 <saurabhkush27@gmail.com>
2025-07-23 23:12:56 +0530

    Added loading for myCource page Fetching

--- app/src/main/java/com/example/testapp/screens/MyCoursesScreen.kt
+++ app/src/main/java/com/example/testapp/screens/MyCoursesScreen.kt
@@ -2,7 +2,6 @@
 package com.example.testapp.screens
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,12 +14,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -51,6 +50,7 @@ fun MyCoursesScreen(
     viewModel: CourseProgressViewModel = viewModel()
 ) {
     val myCourses by viewModel.myCourses.collectAsState()
+    val isLoading by viewModel.isLoading.collectAsState()
 
     LaunchedEffect(Unit) {
         viewModel.fetchMyCoursesWithProgress()
@@ -65,7 +65,15 @@ fun MyCoursesScreen(
                 }
             },
             modifier = Modifier.shadow(8.dp)) }){ padding ->
-        if (myCourses.isEmpty()) {
+        if (isLoading){
+            Box(
+                modifier = Modifier.fillMaxSize().padding(padding),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+        else if(myCourses.isEmpty()){
             Box(
                 modifier = Modifier.fillMaxSize().padding(padding),
                 contentAlignment = Alignment.Center
