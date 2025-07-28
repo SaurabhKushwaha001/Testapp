af2672fccc09e2cbc6c4e527355222cf642a4820
SaurabhKushwaha001 <saurabhkush27@gmail.com>
2025-07-28 19:17:00 +0530

    Added Search Bar

--- app/src/main/java/com/example/testapp/screens/CourseHomeScreen.kt
+++ app/src/main/java/com/example/testapp/screens/CourseHomeScreen.kt
@@ -1,88 +1,169 @@
 package com.example.testapp.screens
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import java.net.URLEncoder
-import java.nio.charset.StandardCharsets
-import androidx.compose.material3.Text
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
 import android.annotation.SuppressLint
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import coil.compose.rememberAsyncImagePainter
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
-import com.example.testapp.viewmodel.CourseViewModel
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
+import com.example.testapp.viewmodel.CourseViewModel
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun CourseHomeScreen(navController: NavController, viewModel: CourseViewModel = viewModel()) {
     val courses by viewModel.courses.collectAsState()
+    var isSearching by remember { mutableStateOf(false) }
+    var searchText by remember { mutableStateOf("") }
+
+    val filteredCourses = if (searchText.isEmpty()) {
+        courses
+    }else {
+        courses.filter { course ->
+            course.title.contains(searchText, ignoreCase = true)
+        }
+    }
     Scaffold(
         topBar = {
-            TopAppBar(
-                title = {
-                    Text(
-                        text = "Courses",
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                },
-                navigationIcon = {
-                    IconButton(onClick = {
-                        navController.navigate("profile")
-                    }) {
-                        Icon(
-                            imageVector = Icons.Filled.Menu,
-                            contentDescription = "Menu"
-                        )
-                    }
-                },
-                modifier = Modifier.shadow(8.dp))
+            if (isSearching) {
+                TopAppBar(
+                    title = {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(end = 18.dp)
+                        ) {
+                            TextField(
+                                value = searchText,
+                                onValueChange = { searchText = it },
+                                placeholder = { Text("Search courses") },
+                                singleLine = true,
+                                shape = RoundedCornerShape(12.dp), // Rounded corners
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(56.dp), // Match TopAppBar height
+                                colors = TextFieldDefaults.colors(
+                                    unfocusedContainerColor = Color.White,
+                                    focusedContainerColor = Color.White,
+                                    unfocusedIndicatorColor = Color.Transparent,
+                                    focusedIndicatorColor = Color.Transparent
+                                ),
+                                trailingIcon = {
+                                    IconButton(
+                                        onClick = {
+                                            if (searchText.isNotEmpty()) {
+                                                isSearching = false
+                                                searchText = ""
+                                            } else {
+                                                isSearching = false
+                                            }
+                                        }
+                                    ) {
+                                        Icon(
+                                            imageVector = Icons.Default.Close,
+                                            contentDescription = "Back"
+                                        )
+                                    }
+                                },
+                                textStyle = MaterialTheme.typography.bodyMedium.copy(fontSize = 16.sp)
+                            )
+                        }
+                    },
+                    navigationIcon = {},
+                    actions = {},
+                    modifier = Modifier.shadow(8.dp)
+                )
+            } else {
+                TopAppBar(
+                    title = {
+                        Text("Courses")
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            navController.navigate("profile")
+                        }) {
+                            Icon(
+                                imageVector = Icons.Filled.Menu,
+                                contentDescription = "Menu"
+                            )
+                        }
+                    },
+                    actions = {
+                        IconButton(onClick = { isSearching = true }) {
+                            Icon(
+                                imageVector = Icons.Default.Search,
+                                contentDescription = "Search"
+                            )
+                        }
+                    },
+                    modifier = Modifier.shadow(8.dp)
+                )
+            }
         }
-    ) { innerPadding ->
+    )  { innerPadding ->
         LazyColumn(modifier = Modifier
             .padding(8.dp)
             .padding(innerPadding)) {
-            items(courses) { course ->
+            items(filteredCourses) { course ->
                 Card(
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(vertical = 8.dp)
                         .clickable {
-                            navController.navigate("CourseOverview/${course.playlistId}/${course.title}/${URLEncoder.encode(course.thumbnailUrl, StandardCharsets.UTF_8.toString())}")
+                            navController.navigate(
+                                "CourseOverview/${course.playlistId}/${course.title}/${
+                                    URLEncoder.encode(
+                                        course.thumbnailUrl,
+                                        StandardCharsets.UTF_8.toString()
+                                    )
+                                }"
+                            )
                         },
-                            colors = CardDefaults.cardColors(containerColor = Color(0xFFF5F5F5)),
-                            shape = RoundedCornerShape(16.dp),
-                            elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+                    colors = CardDefaults.cardColors(containerColor = Color(0xFFF5F5F5)),
+                    shape = RoundedCornerShape(16.dp),
+                    elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
                 ) {
                     Row(modifier = Modifier.padding(8.dp)) {
                         Image(
